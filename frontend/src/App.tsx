042f20dc15d3ee92820e5866bd68782f6f183e52
youssef <youssefeffat1510ye@gmail.com>
2026-05-12 13:44:16 +0200

    feat(auth): add AuthProvider, PrivateRoute; protect routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { Toaster } from 'sonner';
+import { AuthProvider } from './auth';
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <RouterProvider router={router} />
       <Toaster position="top-center" />
-    </>
+    </AuthProvider>
   );
 }
 
